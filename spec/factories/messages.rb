ac13d1ff7c68069770db734758095d0b14057727
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-02 17:10:19 +0900

    ファイルオープン機能

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     content {Faker::Lorem.sentence}
     association :user
     association :room
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
